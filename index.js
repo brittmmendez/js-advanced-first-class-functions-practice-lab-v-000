96dbbeeb6b1a9d55249600c3ff065039ce9351df
Brittany Morris <brittmmendez@gmail.com>
2018-03-17 16:40:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,29 @@ function logDriverNames(drivers){
   drivers.forEach(calback);
 }
 
-
 const calback=function (el,i,array){
   console.log(el.name);
 };
+
+//first way I solved it 
+  //const logDriverNames=function (el,i,array){
+  //  console.log(el.name);
+  //};
+  //  drivers.forEach(logDriverNames)
+//solution in github
+  //  const logDriverNames = function (drivers) {
+  //    drivers.forEach(function (driver) {
+  //      console.log(driver.name);
+  //    });
+  //  };
+
+
+const logDriversByHometown = function (drivers, hometown) {
+  drivers.forEach(function (driver) {
+    if (driver.hometown===hometown;){
+    console.log(driver.name);
+    }
+  });
+};
+
+  
