40ed038d08104bb4c00cbfb699e281a4213c58e1
Brittany Morris <brittmmendez@gmail.com>
2018-03-17 16:32:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const logDriverNames=function (el,i,array){
+function logDriverNames(drivers){
+  drivers.forEach(calback);
+}
+
+
+const calback=function (el,i,array){
   console.log(el.name);
 };
-  drivers.forEach(logDriverNames);
